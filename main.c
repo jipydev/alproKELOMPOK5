3a67c6b9e8880b5de80bc4d09ccdf97ebd5ae9a8
Irayha <dirgaahyari@gmail.com>
2025-12-08 02:21:03 +0700

    Fungsi tambah barang

--- main.c
+++ main.c
@@ -3,13 +3,28 @@
 
 struct inventory
 {
-    int id;
+    char jenis[20];
     char namaBarang[50];
     int stok;
 
 };
 struct inventory barang[MAX];
 int jumlah = 0;
+
+//fungsi nambah barang
+void tambahBarang(){
+printf ("Isi Dengan Seksama Ya\n");
+printf("Masukan Jenis Barang : ");
+scanf("%s",barang[jumlah].jenis);
+printf("Masukan Nama Barang : ");
+scanf("%s",barang[jumlah].namaBarang);
+printf("Masukan stok Barang : ");
+scanf("%s",barang[jumlah].stok);
+
+jumlah++;
+printf("Berhasil Ditambahkan :)\n");
+}
+
 
 
 
