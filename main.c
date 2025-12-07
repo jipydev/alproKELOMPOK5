384b1903cea8ed20d744f3c5007a3e0aaa8b2882
Irayha <dirgaahyari@gmail.com>
2025-12-08 01:54:34 +0700

    struct

--- main.c
+++ main.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
+#define MAX 50
+
+struct inventory
+{
+    int id;
+    char namaBarang[50];
+    int stok;
+
+};
+struct inventory barang[MAX];
+int jumlah = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int main(){
-printf("===============================");
-printf("Kelengkapan Posyandu");
-printf("===============================");
+printf("===============================\n");
+printf("Kelengkapan Posyandu\n");
+printf("===============================\n");
 
 }
