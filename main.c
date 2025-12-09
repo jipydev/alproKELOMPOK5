e330b691d30f3abd9f385905894703f9316dab48
Irayha <dirgaahyari@gmail.com>
2025-12-10 00:23:50 +0700

    fungsi lihat barang beta

--- main.c
+++ main.c
@@ -1,45 +1,59 @@
 #include <stdio.h>
+#include <string.h>
 #define MAX 50
-
 struct inventory
+
 {
     char jenis[20];
-    char namaBarang[50];
+    char namaBarang[20];
     int stok;
 
 };
 struct inventory barang[MAX];
 int jumlah = 0;
-
+char y;
 //nambah barang
 void tambahBarang(){
+
 printf ("Isi Dengan Seksama Ya\n");
-printf("Masukan Jenis Barang : ");
+printf("Masukan Jenis Barang (obat/alat) : ");
 scanf("%s",barang[jumlah].jenis);
 printf("Masukan Nama Barang : ");
 scanf("%s",barang[jumlah].namaBarang);
 printf("Masukan stok Barang : ");
-scanf("%s",barang[jumlah].stok);
+scanf("%d",&barang[jumlah].stok);
 
 jumlah++;
 printf("Berhasil Ditambahkan :)\n");
+printf("Tambahkan Lagi Y/T : ");
+scanf(" %c",&y);
+
+while (y!='T'){
+printf ("Isi Dengan Seksama Ya\n");
+printf("Masukan Jenis Barang (obat/alat) : ");
+scanf("%s",barang[jumlah].jenis);
+printf("Masukan Nama Barang : ");
+scanf("%s",barang[jumlah].namaBarang);
+printf("Masukan stok Barang : ");
+scanf("%d",&barang[jumlah].stok);
+
+jumlah++;
+printf("Berhasil Ditambahkan :)\n");
+printf("Tambahkan Lagi Y/T : ");
+scanf(" %c",&y);
+}
+}
+//liat barang
+void liatbarang(){
+for ( int i= 0; i<jumlah; i++){
+    printf(" \nJenis Barang : %s \n",barang[i].jenis);
+    printf(" Jenis Nama Barang : %s \n",barang[i].namaBarang);
+    printf(" Jenis Stok : %d \n",barang[i].stok);
+    
 }
 
-//lihat barang
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
@@ -50,8 +64,8 @@ printf("Berhasil Ditambahkan :)\n");
 
 
 int main(){
-printf("===============================\n");
-printf("Kelengkapan Posyandu\n");
-printf("===============================\n");
 
+tambahBarang();
+liatbarang();
 }
+
