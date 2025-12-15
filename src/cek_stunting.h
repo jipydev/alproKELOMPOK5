e18833ccb67220728a62ac4e61993ae48fa1d90a
FajarRamadhanSudendi <ramadhansudendi321@gmail.com>
2025-12-15 14:56:51 +0700

    CekStunting

---
+++ src/cek_stunting.h
@@ -0,0 +1,18 @@
+#ifndef CEK_STUNTING_H
+#define CEK_STUNTING_H
+
+/* Menu utama */
+void menuStunting();
+
+/* CRUD */
+void tambahStunting();
+void lihatStunting();
+void editStunting();
+void hapusStunting();
+
+/* Submenu lihat */
+void tampilkanSemuaStunting();
+void cariStuntingById();
+void cariStuntingByNama();
+
+#endif
