73dbe1f06e81881189216f7dc36269db856d0b07
danyyfad <danyfadhilahahnaf@gmail.com>
2025-12-15 15:18:17 +0700

    coding rekam medis 2

---
+++ src/rekam_medis.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "rekam_medis.h"
+#include "utils.h"
+
+#define FILE_REKAM "db/rekam_medis.txt"
+#define FILE_TEMP  "db/temp.txt"
+
