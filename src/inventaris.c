961d3f356fb3ae206e1aecc265d90af675217137
Irayha <dirgaahyari@gmail.com>
2025-12-17 18:58:43 +0700

    structcinv

---
+++ src/inventaris.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Inventaris.h"
+#include "utils.h"
+
+
+#define FILE_INV  "inventaris.txt"
+
+typedef struct 
+{
+    int id;
+    char namaBarang[50];
+    int stok;
+}inventaris;
+
+//Fitur tambah 
+void tambahinv(){
+
+    
+}
+
+//Menu utama 
+void menuInventaris(){
+
+clearScreen();
+printf("|=========================|\n");
+printf("|       Inventory         |\n");
+printf("|=========================|\n");
+printf("|1.Lihat Barang           |\n");
+printf("|-------------------------|\n");
+printf("|2.Tambah Barang          |\n");
+printf("|-------------------------|\n");
+printf("|3.Edit Barang            |\n");
+printf("|-------------------------|\n");
+printf("|4.Cari Barang            |\n");
+printf("|-------------------------|\n");
+printf("|5.Hapus Barang           |\n");
+printf("|-------------------------|\n");
+}
+
+void tambahInv(){
+f
+
+
+}
+
+
+
